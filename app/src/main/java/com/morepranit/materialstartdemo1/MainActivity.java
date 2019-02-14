98a21eed8337abb47f92c38c725c92d437b23d19
Pranit <pranit@fierydevs.com>
2019-02-14 10:30:03 +0530

    Initial Commit

---
+++ app/src/main/java/com/morepranit/materialstartdemo1/MainActivity.java
@@ -0,0 +1,18 @@
+package com.morepranit.materialstartdemo1;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
